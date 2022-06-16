646379e5699242f602e94743c22a00c12ac3f4ec
riandwisusanto <riandwisusanto03@gmail.com>
2022-06-16 14:54:22 +0700

    done v1.1

--- app.py
+++ app.py
@@ -28,20 +28,39 @@ def lsb(image, secret_data, prn):
 
         pixel = image[0][position1]
         r,g,b = data2binary(pixel)
-        
-        if position1 == 545:
-            r,g,b = data2binary(image[0][545])
 
         image[0][position1][0] = int(r[:-1] + binary, 2)
         image[0][position1][1] = int(g[:-1] + binary, 2)
         image[0][position1][2] = int(b[:-1] + binary, 2)
-
-        if position1 == 545:
-            r,g,b = data2binary(image[0][545])
             
         index += 1
 
     return image
+
+# insert lsb in image
+def lsb_to_bin(image, secret_data, prn): 
+    secret_data += "#####" 
+
+    binary_data = data2binary(secret_data)
+
+    im    = []
+    index = 0
+    for binary in binary_data:
+        position1 = prn[index]
+        pixel = image[0][position1]
+        r,g,b = data2binary(pixel)
+
+        ar = [
+            r[:-1] + binary,
+            g[:-1] + binary,
+            b[:-1] + binary
+        ]
+
+        im.append(ar)
+            
+        index += 1
+
+    return im
 
 # convert key and data to chippertext with ksa and prga method
 def rc4(key, data):
@@ -76,45 +95,94 @@ def home():
 
 @app.route('/encode', methods=['POST'])
 def encode():
+    # pra proses 1
     time_execute = time.time()
     picture = request.files['picture']
     savepath = os.path.join("assets/img/"+picture.filename)
     picture.save(savepath)
 
+    # pra proses 2
     image = cv2.imread("assets/img/" + picture.filename)
     message  = request.form['hidden_text']
     password = request.form['password']
 
+    # convert prng and rc4
     position = prng(password, image.shape[0])
     chipper  = rc4(password, message)
 
+    # encode to lsb and save to img_lsb as image
     encode  = lsb(image, chipper, position)
     cv2.imwrite("assets/img_lsb/"+ picture.filename.split('.')[0] + '.png' ,encode)
 
-    ori_img = [
-                picture.filename, 
-                os.path.getsize('assets/img/'+picture.filename)
-            ]
-    lsb_img = [
-                picture.filename.split('.')[0] + '.png',
-                os.path.getsize('assets/img_lsb/'+picture.filename.split('.')[0] + '.png')
-            ]
+    # display array image and array image lsb
+    ar_ori = cv2.imread("assets/img/" + picture.filename)
+    ar_lsb = cv2.imread("assets/img_lsb/" + picture.filename.split('.')[0] + '.png')
+    arr_ori = []
+    arr_lsb = []
+    bin_ori = []
+    bin_lsb = []
+    i = 0
+    for pos in position:
+        arr_ori.append(ar_ori[0][pos])
+        arr_lsb.append(ar_lsb[0][pos])
+
+        bin_ori.append(data2binary(ar_ori[0][pos]))
+        bin_lsb.append(data2binary(ar_lsb[0][pos]))
+
+        i += 1
+        if i == (len(message) * 8):
+            break
+
+    # set ori image to display result
+    ori_img = {
+        "path": picture.filename,
+        "size": os.path.getsize('assets/img/'+picture.filename),
+        "arr" : arr_ori,
+        "bin" : bin_ori
+    }
+    
+    # set lsb image to display result
+    lsb_img = {
+        "path": picture.filename.split('.')[0] + '.png',
+        "size": os.path.getsize('assets/img_lsb/'+picture.filename.split('.')[0] + '.png'),
+        "arr" : arr_lsb,
+        "bin" : bin_lsb
+    }
     
     time_execute = time.time() - time_execute
 
-    return render_template("encode.html", ori_img = ori_img, lsb_img = lsb_img, time_execute = time_execute)
+    # compress all to one variabel
+    data = {
+        # proses
+        "text"   : message,
+        "pass"   : password,
+        "tahap1" : ', '.join([str(elem) for elem in position]),
+        "tahap2" : chipper,
+        "tahap3" : data2binary(chipper),
+        "tahap4" : ar_ori[0],
+        "tahap5" : [data2binary(elem) for elem in ar_ori[0]],
+        "tahap6" : lsb_to_bin(ar_ori, "pppppp", position),
+        # hasil
+        "ori_img": ori_img,
+        "lsb_img": lsb_img,
+        "time_execute": time_execute
+    }
+
+    return render_template("encode.html", data = data)
 
 @app.route('/decode', methods=['GET','POST'])
 def decode():
     time_execute = time.time()
     dekrip = ""
     status = "get"
+    path   = ""
 
     if request.method == 'POST':
         password = request.form['password']
         picture  = request.files['picture']
 
         picture = request.files['picture']
+        path    = picture.filename
         savepath = os.path.join("assets/temp_"+picture.filename)
         picture.save(savepath)
 
@@ -147,8 +215,8 @@ def decode():
 
         time_execute = time.time() - time_execute
         
-    return render_template("decode.html", dekrip_text = dekrip, status = status, time_execute = time_execute)
+    return render_template("decode.html", path = path, dekrip_text = dekrip, status = status, time_execute = time_execute)
 
 if __name__ == '__main__':
     webbrowser.open('http://127.0.0.1:5000/')
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
